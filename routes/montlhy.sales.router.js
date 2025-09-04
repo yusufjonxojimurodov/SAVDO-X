460da75f21699f545d057d86bec9231ccca6eab4
Yusuf Xojimurodov <xojimurodovyusuf7@gmail.com>
2025-09-04 16:21:57 +0500

    test

--- routes/montlhy.sales.router.js
+++ routes/montlhy.sales.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const MonthlySale = require("../models/monthlySale.model");
+const MonthlySale = require("../models/montlhy.sale.model");
 
 const JWT_TOKEN = process.env.JWT_TOKEN;
 
